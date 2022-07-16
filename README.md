d6c740e0db17d8ba502e83a0aec7513b72279e6a
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2022-07-16 15:09:54 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ZendDB [![Build Status](https://app.travis-ci.com/etienne-dldc/zendb-node.svg)](https://travis-ci.com/etienne-dldc/zendb-node)
+# ZendDB Better-SQLite3 [![Build Status](https://app.travis-ci.com/etienne-dldc/zendb-better-sqlite3.svg)](https://travis-ci.com/etienne-dldc/zendb-better-sqlite3)
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,80 @@
-export * from './schema';
-export * from './Expr';
-export * from './Migrations';
-export * from './Database';
-export * from './DatabaseTable';
+import { IDriver, IDriverDatabase, IDriverStatement } from 'zendb';
+import Database from 'better-sqlite3';
+
+export class Driver implements IDriver<DriverDatabase> {
+  connect(path: string): DriverDatabase {
+    return new DriverDatabase(new Database(path));
+  }
+
+  remove(_path: string): void {
+    throw new Error('Method not implemented.');
+  }
+
+  rename(_oldPath: string, _newPath: string): void {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class DriverDatabase implements IDriverDatabase<DriverStatement> {
+  public readonly db: Database.Database;
+
+  constructor(db: Database.Database) {
+    this.db = db;
+  }
+
+  prepare(source: string): DriverStatement {
+    return new DriverStatement(this.db.prepare(source));
+  }
+
+  transaction(_fn: () => void): void {
+    throw new Error('Method not implemented.');
+  }
+
+  exec(source: string): this {
+    this.db.exec(source);
+    return this;
+  }
+
+  close(): void {
+    this.db.close();
+  }
+
+  getUserVersion(): number {
+    return this.db.pragma('user_version', { simple: true });
+  }
+
+  setUserVersion(version: number): void {
+    this.db.pragma(`user_version = ${version}`, { simple: true });
+  }
+}
+
+export class DriverStatement implements IDriverStatement {
+  public readonly statement: Database.Statement;
+
+  constructor(statement: Database.Statement) {
+    this.statement = statement;
+  }
+
+  run(...params: any[]): { changes: number } {
+    const res = this.statement.run(...params);
+    return { changes: res.changes };
+  }
+
+  get(...params: any[]) {
+    return this.statement.get(...params);
+  }
+
+  all(...params: any[]): any[] {
+    return this.statement.all(...params);
+  }
+
+  bind(...params: any[]): this {
+    this.statement.bind(...params);
+    return this;
+  }
+
+  free() {
+    // Do nothing
+    return;
+  }
+}
